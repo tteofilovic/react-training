e51b61d38354fef473f9cb4e682280abb5a885b7
Toni Teofilovic <20360072+tteofilovic@users.noreply.github.com>
2019-01-08 12:39:23 +0100

    fix

--- src/reducers/opacity.js
+++ src/reducers/opacity.js
@@ -1,15 +1,14 @@
 import * as types from '../actions/types';
 
 const initialState = {
-    opacity:1
+    1
 };
 
 const opacity = (state = initialState, action) => {
     switch (action.type){
         case types.CHANGE_OPACITY:
             return {
-                ...state,
-                opacity: action.payload
+                action.payload
             }
         default:
             return state;
